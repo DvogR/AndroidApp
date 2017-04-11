f295272cb7c651662799c5d7b19c38ef4fca988d
Alex <alex@gmail.com>
2017-04-11 14:23:47 +0300

    Add new display

--- app/src/main/java/com/example/test/test/MainActivity.java
+++ app/src/main/java/com/example/test/test/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.test.test;
 
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, Title.class);
+        startActivity(intent);
     }
 }
